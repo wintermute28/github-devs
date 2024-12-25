62626b3f72d955e2811cf0f98a0546e32b6150f5
wintermute28 <a28c@yandex.ru>
2024-12-25 14:23:53 +0300

    Add Search

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Container } from "components/Container";
+import { Search } from "components/Search";
 import { TheHeader } from "components/TheHeader";
 function App() {
   return (
     <Container>
       <TheHeader />
+      <Search hasError onSubmit={() => {}} />
     </Container>
   );
 }
