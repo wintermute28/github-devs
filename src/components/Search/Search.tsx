ea422bdc9c5c8f8b0feeebf63f46e92b93200176
wintermute28 <a28c@yandex.ru>
2024-12-26 09:11:08 +0300

    Make Search without ref

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,5 @@
 import { ReactComponent as SearchIcon } from "assets/icon-search.svg";
 import styles from "./Search.module.scss";
-import { useRef } from "react";
 import { Button } from "components/Button";
 
 interface SearchProps {
@@ -8,19 +7,20 @@ interface SearchProps {
   onSubmit: (text: string) => void;
 }
 
+type FormFields = {
+  username: HTMLInputElement;
+};
+
 export const Search = ({ hasError, onSubmit }: SearchProps) => {
-  const searchRef = useRef<HTMLInputElement | null>(null);
-
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = (
+    event: React.FormEvent<HTMLFormElement & FormFields>
+  ) => {
     event.preventDefault();
-
-    const text = searchRef.current ? searchRef.current.value : "";
+    const text = event.currentTarget.username.value;
 
     if (text) {
       onSubmit(text);
-      if (searchRef.current) {
-        searchRef.current.value = "";
-      }
+      event.currentTarget.reset();
     }
   };
 
@@ -31,7 +31,6 @@ export const Search = ({ hasError, onSubmit }: SearchProps) => {
           <SearchIcon />
         </label>
         <input
-          ref={searchRef}
           type="text"
           className={styles.textField}
           id="search"
