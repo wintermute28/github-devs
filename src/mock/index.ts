d9dc5dc2505673843496d982c1ff9396001e0a03
wintermute28 <a28c@yandex.ru>
2024-12-26 13:18:02 +0300

    Add fetchUser

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -2,14 +2,13 @@ import { LocalGithubUser } from "types";
 
 export const defaultUser: LocalGithubUser = {
   login: "wintermute28",
-  id: 101113080,
   avatar: "https://avatars.githubusercontent.com/u/101113080?v=4",
   name: "Sergey S.",
-  company: "MyCompany",
+  company: "",
   blog: "https://webbeat.ru/",
   location: "Moscow",
   bio: "Frontend developer",
-  twitter: "wintermute28",
+  twitter: "",
   repos: 26,
   followers: 0,
   following: 0,
